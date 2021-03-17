e15282db3659a72cd53fc12c4285b2d12eeafde8
Valdecir Waldhelm Morgado <valdwald81@gmail.com>
2021-03-17 15:42:58 -0300

    DIOBank
    Projeto ainda em formação com .Net C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIObank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Jõao das Coves");
+
+
+            Console.WriteLine(minhaConta.ToString());
+        }
+    }
+}
